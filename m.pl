4a2d178bf17f8210cea87a2263f57090793c4399
Junxian Huang <junxian.huang@gmail.com>
2013-01-26 03:56:52 -0500

    Update

--- m.pl
+++ m.pl
@@ -3,5 +3,5 @@ use warnings;
 
 print("compiling ...\n");
 system("rm run.exe");
-system("g++ data.cpp main.cpp bw.cpp util.cpp -lz -I /home/hjx/libpcap-1.3.0/ /home/hjx/libpcap-1.3.0/libpcap.so.1.3.0 -o ./run.exe");
+system("g++ data.cpp main.cpp tcp_flow.cpp user.cpp util.cpp -lz -I /home/hjx/libpcap-1.3.0/ /home/hjx/libpcap-1.3.0/libpcap.so.1.3.0 -o ./run.exe");
 
